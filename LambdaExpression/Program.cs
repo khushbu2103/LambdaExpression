42c43dec8883fe76f072723c7a952a6e5361a0ac
khushbu <khushbusoni475@gmail.com>
2023-06-15 19:20:04 +0530

    [khushbu] modify Person Data Management

--- LambdaExpression/Program.cs
+++ LambdaExpression/Program.cs
@@ -11,7 +11,26 @@ namespace LambdaExpression
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            List<Person> list = new List<Person>()
+            {
+                new Person(){SSN=1, Name="Khushi", Address="Rajasthan", Age=26},
+                new Person(){SSN=1, Name="Raj", Address="gujrat", Age=28},
+                new Person(){SSN=1, Name="Harish", Address="Delhi", Age=20},
+                new Person(){SSN=1, Name="Thia", Address="Mumbai", Age=21},
+                new Person(){SSN=1, Name="Ramesh", Address="Pune", Age=62},
+
+            };
+            DisplayPersonDeta(list);
             Console.ReadLine();
+        }
+
+        public static void DisplayPersonDeta(List<Person> list)
+        {
+            foreach(Person p in list)
+            {
+                Console.WriteLine(p.ToString());
+            }
         }
     }
 }
